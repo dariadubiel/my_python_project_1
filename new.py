314dc203036c07a93d2b44379eaf11e3c02db121
Daria Dubiel <dariaszatara@gmail.com>
2017-11-05 18:58:20 +0100

    current  date printing

---
+++ new.py
@@ -0,0 +1,9 @@
+import datetime
+
+filename=datetime.datetime.now()
+
+def create_file():
+    with open(filename.strftime("%Y-%m-%d-%H-%M")+".txt", "w") as file:
+         file.write("")
+
+create_file()
